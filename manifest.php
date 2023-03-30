3377535f3140e1710f4133ef4b75ea49bf4e992b
Leon Nikitin <nlv@lab321.ru>
2023-03-30 16:44:42 +0600

    Добавили пересчет плана для продукта

--- manifest.php
+++ manifest.php
@@ -12,13 +12,13 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin (nlv@lab321.com)',
     'description' => 'Добавление ERP функционала в SuiteCRM',
     'is_uninstallable' => true,
-    'published_date' => '2023-03-28',
+    'published_date' => '2023-03-30',
     'type' => 'module',
-    'version' => '0.2.1',
+    'version' => '0.3.0',
   'dependencies' => array(
     array(
       'id_name' => 'hs321_erp',
-      'version' => '0.2',
+      'version' => '0.3',
     ),
   ),
 );
