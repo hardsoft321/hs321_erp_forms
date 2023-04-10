7054f23f726ec4d7ccc891a1e653e1df72ccaf93
Leon Nikitin <nlv@lab321.ru>
2023-04-10 11:19:45 +0600

    Добавлен отчет прогноза движения товара

--- manifest.php
+++ manifest.php
@@ -12,13 +12,13 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin (nlv@lab321.com)',
     'description' => 'Добавление ERP функционала в SuiteCRM',
     'is_uninstallable' => true,
-    'published_date' => '2023-04-03',
+    'published_date' => '2023-04-07',
     'type' => 'module',
-    'version' => '0.4.0',
+    'version' => '0.5.0',
   'dependencies' => array(
     array(
       'id_name' => 'hs321_erp',
-      'version' => '0.4',
+      'version' => '0.5',
     ),
   ),
 );
