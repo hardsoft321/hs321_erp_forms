10161ec077dde1ca2e9be3707fcc84ca2764c450
Leon Nikitin <nlv@lab321.ru>
2023-04-14 11:38:18 +0600

    При создании договора из квоты вызывается createContract из модуля AOS_ERP

--- manifest.php
+++ manifest.php
@@ -12,13 +12,13 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin (nlv@lab321.com)',
     'description' => 'Добавление ERP функционала в SuiteCRM',
     'is_uninstallable' => true,
-    'published_date' => '2023-04-12',
+    'published_date' => '2023-04-14',
     'type' => 'module',
-    'version' => '0.6.0',
+    'version' => '0.7.0',
   'dependencies' => array(
     array(
       'id_name' => 'hs321_erp',
-      'version' => '0.6',
+      'version' => '0.7',
     ),
   ),
 );
